63982de46e3687cf92442d6f5fe2e4f4115c49bc
Dhaerya <dhaeryamore@gmail.com>
2025-04-17 19:26:49 +0530

    Handled thread joining and exception handling in Main.java

--- Main.java
+++ Main.java
@@ -3,17 +3,29 @@
 // Batch: A2
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	System.out.println("Main thread started.");
+		System.out.println("Main thread started.");
 
-	// Creating thread objects
-	Thread t1 = new Thread(new PrintNumbers());
-	Thread t2 = new Thread(new PrintAlphabets());
+		// Creating thread objects
+		Thread t1 = new Thread(new PrintNumbers());
+		Thread t2 = new Thread(new PrintAlphabets());
 
-	// Starting threads
-	t1.start();
-	t2.start();
+		// Starting threads
+		t1.start();
+		t2.start();
 
-    }
+		// Add this after t2.start();
+
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			System.out.println("Thread interrupted: " + e.getMessage());
+		}
+
+		System.out.println("Main thread finished.");
+
+	}
 }
+
