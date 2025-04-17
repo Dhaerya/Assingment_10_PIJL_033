b3983e79890427259c903a06a52cbfe6f632a558
Dhaerya <dhaeryamore@gmail.com>
2025-04-17 19:25:11 +0530

    Added thread initialization and starting logic in Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main thread started.");
+
+	System.out.println("Main thread started.");
+
+	// Creating thread objects
+	Thread t1 = new Thread(new PrintNumbers());
+	Thread t2 = new Thread(new PrintAlphabets());
+
+	// Starting threads
+	t1.start();
+	t2.start();
+
     }
 }
