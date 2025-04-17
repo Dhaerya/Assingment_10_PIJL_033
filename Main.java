25e816ccdd0a0801f6a9de18eccb06fdaf9e627f
Dhaerya <dhaeryamore@gmail.com>
2025-04-17 19:23:31 +0530

    Created Main.java with class structure and basic setup

---
+++ Main.java
@@ -0,0 +1,9 @@
+// Name: Dhaerya
+// PRN: 23070126033
+// Batch: A2
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main thread started.");
+    }
+}
